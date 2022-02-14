ed87e5204d1a96356f946fc0c4731fa070c3bca9
Luiz Tadeu Mendonça <tadeumen@amazon.com>
2022-02-14 17:10:08 -0300

    Testing userpool with custom domain

--- cognito-httpapi-customdomain/src/redirect/app.js
+++ cognito-httpapi-customdomain/src/redirect/app.js
@@ -1,13 +1,27 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+const axios = require('axios');
+const qs = require('qs');
 
 exports.handler = async (event) => {
+    let clientId = process.env.ClientId;
+    let tokenUrl = process.env.TokenUrl;
     let redirectUrl = process.env.RedirectUrl;
-    const response = {
-        statusCode: 302,
-        headers: {
-            Location: redirectUrl
-        }
-    };
-    return response;
+    let authorizationCode = event.queryStringParameters.code;
+    
+    // Get Token with Auth Code
+    axios({
+        method: 'POST',
+        url: tokenUrl,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify({
+            grant_type: 'authorization_code',
+            client_id: clientId,
+            redirect_uri: redirectUrl
+        })
+    })
+    .then(function (response) {
+        response.data
+    });
+    return event;
 };
