64c4c54f4dfcdc70c08c9dc9afd889a12ef3b16f
Luiz Tadeu Mendonça <tadeumen@amazon.com>
2022-02-14 14:17:52 -0300

    Login Route

---
+++ cognito-httpapi-customdomain/src/login/app.js
@@ -0,0 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+exports.handler = function (event, context) {
+	context.succeed('hello world');
+};
