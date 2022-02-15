b480a51da69d14317bf5fc0051aecd8c81c5a5c5
Luiz Tadeu Mendonça <tadeumen@amazon.com>
2022-02-15 13:27:55 -0300

    Deadend - Token Redirect

--- cognito-httpapi-customdomain/src/login/app.js
+++ cognito-httpapi-customdomain/src/login/app.js
@@ -1,14 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-exports.handler = async (event) => {
+exports.handler = async (event,context) => {
     let baseAuthUrl = process.env.AuthUrl;
     let clientId = process.env.ClientId;
     let redirectUrl = process.env.RedirectUrl;
     const response = {
         statusCode: 302,
         headers: {
-            Location: `${baseAuthUrl}?response_type=code&client_id=${clientId}&redirect_uri=${redirectUrl}&state=STATE&scope=email`
+            Location: `${baseAuthUrl}?response_type=token&client_id=${clientId}&redirect_uri=${redirectUrl}&state=STATE&scope=email`
         }
     };
     return response;
--- cognito-httpapi-customdomain/src/redirect/app.js
+++ cognito-httpapi-customdomain/src/redirect/app.js
@@ -1,27 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
-const axios = require('axios');
-const qs = require('qs');
 
-exports.handler = async (event) => {
+exports.handler = async (event,context) => {
     let clientId = process.env.ClientId;
     let tokenUrl = process.env.TokenUrl;
     let redirectUrl = process.env.RedirectUrl;
-    let authorizationCode = event.queryStringParameters.code;
-    
-    // Get Token with Auth Code
-    axios({
-        method: 'POST',
-        url: tokenUrl,
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data: qs.stringify({
-            grant_type: 'authorization_code',
-            client_id: clientId,
-            redirect_uri: redirectUrl
-        })
-    })
-    .then(function (response) {
-        response.data
-    });
+    // let accessToken = event.queryStringParameters.access_token;
+    // console.log(`AccessToken: ${accessToken}`)
+    console.log(context);
     return event;
 };
